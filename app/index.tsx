56f5daf78bc70083d834ce9a6cb29cc911ad4705
Isaac Pontes <isaaclfp@outlook.com>
2025-05-13 20:41:26 -0300

    Aula 7: Renderização de listas com FlatList e ScrollView

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,43 @@ import PrimaryButton from "@/components/PrimaryButton";
 import { TransactionsModal } from "@/components/TransactionsModal";
 import { globalStyles } from "@/styles/global";
 import { useState } from "react";
-import { Image, StatusBar, Text, View } from "react-native";
+import { FlatList, Image, ListRenderItem, StatusBar, Text, View } from "react-native";
 
-const transactions = [
+type Transaction = {
+  id: string
+  description: string
+  amount: number
+}
+
+const transactions: Transaction[] = [
   { id: '1', description: 'Supermercado', amount: -50.75 },
   { id: '2', description: 'Salário', amount: 2500.00 },
   { id: '3', description: 'Restaurante', amount: -120.40 },
   { id: '4', description: 'Aluguel', amount: -800.00 },
+  { id: '5', description: 'Ração do cachorro', amount: -199.99 },
+  { id: '6', description: 'Ida ao cinema', amount: -54.78 },
+  { id: '7', description: 'Freela', amount: 1600.00 },
+  { id: '8', description: 'Contas de luz', amount: -252.91 },
+  { id: '9', description: 'Conta de água', amount: -80 }
 ];
+
+const renderTransaction: ListRenderItem<Transaction> = ({ item }) => {
+  return (
+    <View style={globalStyles.transactionItem}>
+      <Text style={globalStyles.transactionText}>
+        {item.description}
+      </Text>
+      <Text
+        style={[
+          globalStyles.transactionAmount,
+          item.amount > 0 ? globalStyles.income : globalStyles.expense,
+        ]}
+      >
+        R$ {item.amount}
+      </Text>
+    </View>
+  )
+}
 
 export default function Index() {
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -53,21 +82,11 @@ export default function Index() {
         Transações Recentes
       </Text>
 
-      {transactions.map(transaction => (
-        <View key={transaction.id} style={globalStyles.transactionItem}>
-          <Text style={globalStyles.transactionText}>
-            {transaction.description}
-          </Text>
-          <Text
-            style={[
-              globalStyles.transactionAmount,
-              transaction.amount > 0 ? globalStyles.income : globalStyles.expense,
-            ]}
-          >
-            R$ {transaction.amount}
-          </Text>
-        </View>
-      ))}
+      <FlatList
+        data={transactions}
+        keyExtractor={(item) => item.id}
+        renderItem={renderTransaction}
+      />
     </View>
   );
 }
